1a34f3941376f7c3047ccb07a619e2836330f837
Andriy <andkulak@gmail.com>
2016-03-26 01:08:34 -0400

    added youtube api and some basic html/react files

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,1 +1,26 @@
+import React, { Component } from 'react';
 
+class SearchBar extends Component { //component provides a bunch of functionality to class
+	constructor(props) {
+		super(props);
+
+		this.state = {term: ''};
+	}
+
+	render() { // always must define a render method
+		return (
+			<div>
+		 		<input
+		 			value={this.state.term} // this
+		 			onChange={event => this.setState({term: event.target.value})} /> 
+		 		Value of input: {this.state.term}
+	 		</div>
+		);
+	}
+
+	onInputChange(event) { //event handler. 'event' is what is rendered from input above
+		console.log(event.target.value);
+	}
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react'; // if it's a librabry we installed we don't need to show path directory
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyDEkMhJrsf2f595cIc9r9oYnPIztzW0BJM';
 
 // Create a new component. This component should produce some html
 
-const App = () => {
-	return  <div>Hi </div>;
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+			this.setState({ videos});
+			//above equivalent to this.setState({videos: videos})
+		});
+	}
+
+	render() {
+		 	return (
+			<div>
+				<SearchBar />
+				<VideoDetail video={this.state.videos[0]} />
+				<VideoList videos={this.state.videos} />
+			</div>
+			);
+	}
 }
 
 // Take this components generates html and put it on the page
